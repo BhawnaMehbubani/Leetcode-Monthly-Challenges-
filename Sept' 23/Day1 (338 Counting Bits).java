7495fa15c93addf4adca696a9ea81f5f1322940d
Bhawna Bharat Mehbubani <bhawnamehbubani@gmail.com>
2023-10-01 23:20:20 +0530

    Update Day1 (338 Counting Bits).java

--- Sept' 23/Day1 (338 Counting Bits).java
+++ Sept' 23/Day1 (338 Counting Bits).java
@@ -1,19 +1,12 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh18000\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+class Solution {
+  public int[] countBits(int n) {
+    // Let f(i) := i's # of 1's in bitmask
+    // f(i) = f(i / 2) + i % 2
+    int[] ans = new int[n + 1];
 
-\f0\fs24 \cf0 class Solution \{\
-  public int[] countBits(int n) \{\
-    // Let f(i) := i's # of 1's in bitmask\
-    // f(i) = f(i / 2) + i % 2\
-    int[] ans = new int[n + 1];\
-\
-    for (int i = 1; i <= n; ++i)\
-      ans[i] = ans[i / 2] + (i % 2 == 0 ? 0 : 1);\
-\
-    return ans;\
-  \}\
-\}}
+    for (int i = 1; i <= n; ++i)
+      ans[i] = ans[i / 2] + (i % 2 == 0 ? 0 : 1);
+
+    return ans;
+  }
+}}
