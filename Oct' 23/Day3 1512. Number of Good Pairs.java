6149830e1f5c4e5efebc01c5997ab4b2bddf713e
Bhawna Mehbubani <bhawnamehbubani@gmail.com>
2023-10-03 21:43:22 +0530

    Create Day3 1512. Number of Good Pairs.java

---
+++ Oct' 23/Day3 1512. Number of Good Pairs.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int numIdenticalPairs(int[] nums) {
+
+    // Calculate the frequency of each number
+    int[] count = new int[102];
+
+    for (int num : nums) {
+      count[num]++;
+    }
+
+    int totalCount = 0;
+
+    // Caclulate total number of pairs possible
+    for (int i : count) {
+      totalCount += ((i) * (i-1))/2;
+    }
+
+    return totalCount;
+  }
+}
